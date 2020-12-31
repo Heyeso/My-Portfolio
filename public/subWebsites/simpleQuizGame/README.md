8472caa3db5d9744865e65ab07ca932bab9c4f2a
Abdulsalam Oetayo <adeodetayo92@gmail.com>
2020-12-31 01:31:07 -0500

    Doc, Feat: Updated README.md and manifest.json. Updated WeatherApp Subfolder. Added Project code link to github. Private projects dont have link

---
+++ public/subWebsites/simpleQuizGame/README.md
@@ -0,0 +1,24 @@
+# Simple Quiz Game
+
+This project is a simple attempt to replicate a full working quiz game website with a "fun" theme.
+
+## `Link`
+
+https://myportfolio-e35e6.web.app/subWebsites/simpleQuizGame/index.html
+
+### `Library`
+
+JQuery\
+Font Awesome
+
+### `Media`
+
+Mobile\
+Desktop
+
+### `Web Browser`
+
+(Default) Google Chrome\
+Microsoft Edge\
+Opera
+
--- src/component/projects.js
+++ src/component/projects.js
@@ -1,5 +1,4 @@
 
-
 const Projects = () => {
 
     let proj = [
@@ -7,25 +6,29 @@ const Projects = () => {
             img : "images/subWebsite/quickSearch_Logo.png",
             alt : "quick search application",
             link : "https://quicksearch.web.app",
-            info : "Quick Search - Online dictionary made easy"
+            info : "Quick Search - Online dictionary made easy",
+            code : ""
         },
         {
             img : "images/subWebsite/weatherAppLogo.png",
             alt : "weather application",
             link : "subWebsites/weatherApp_1.0.0/index.html",
-            info : "WeatherApp - Current weather and temperature of any city in the world"
+            info : "WeatherApp - Current weather and temperature of any city in the world",
+            code : "https://github.com/Heyeso/Weather-App"
         },
         {
             img : "images/subWebsite/ad.png",
             alt : "Advertisement template",
             link : "subWebsites/onePageAdvert/index.html",
-            info : "Advertisement Template - Single page web page for service advert"
+            info : "Advertisement Template - Single page web page for service advert",
+            code : "https://github.com/Heyeso/OnePage-Advert"
         },
         {
             img : "images/subWebsite/quizLogo.png",
             alt : "a very short quiz game",
             link : "subWebsites/simpleQuizGame/index.html",
-            info : "Quiz? - Try a very short quiz"
+            info : "Quiz? - Try a very short quiz",
+            code : "https://github.com/Heyeso/Simple-Quiz"
         }
     ]
 
@@ -42,6 +45,7 @@ const Projects = () => {
                     </div>
                     <div className="info">
                         <div>{val.info}</div>
+                        {val.code === "" ? <div className="priv">Private <i class="fas fa-lock"></i></div> : <a rel="noopener noreferrer" href={val.code} target="_blank">Code</a>}
                     </div>
                 </div>
                 ))}
